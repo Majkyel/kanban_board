b933e5dee3d54a9c69728db35161d4edde7c84a7
Michał Kuliś <michalkulis1993@gmail.com>
2018-06-21 22:40:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    var addColumnModal = document.getElementById('AddColumnModal');
+    var addCardModal = document.getElementById('AddCardModal');
+    var closeColumnModal = document.getElementsByClassName('close')[0];
+    var closeCardModal = document.getElementsByClassName('close')[1];
+
     function randomString() {
         var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
         var str = '';
@@ -10,17 +15,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         return str;
     }
-    
+
     function randomColorCard() {
-        var colorTable = ['darkgray','#54a0ff','#f368e0','#ee5253'];
+        var colorTable = '0123456789abcdef';
         var colorTableLength = colorTable.length;
         var randomColor = '';
-        for(var i = 0; i < colorTableLength; i++) {
-            randomColor = colorTable[Math.floor(Math.random() * colorTableLength)];
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 2; j++) {
+                randomColor += colorTable[Math.floor(Math.random() * colorTableLength)];
+            }
         }
         return randomColor;
     }
-    console.log(randomColorCard());
+    randomColorCard();
 
     function generateTemplate(name, data, basicElement) {
         var template = document.getElementById(name).innerHTML;
@@ -31,19 +38,49 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function Column(name) {
-        
+
         var self = this;
         this.id = randomString();
         this.name = name;
-        
-        this.element = generateTemplate('column-template', {name: this.name, id: this.id});
-        
+
+        this.element = generateTemplate('column-template', {
+            name: this.name,
+            id: this.id
+        });
+
         this.element.querySelector('.column').addEventListener('click', function (event) {
+
             if (event.target.classList.contains('btn-delete')) {
                 self.removeColumn();
             }
             if (event.target.classList.contains('add-card')) {
-                self.addCard(new Card(prompt("Enter the name of the card")));
+
+                addCardModal.style.display = 'block';
+
+                closeCardModal.onclick = function () {
+                    addCardModal.style.display = 'none';
+                }
+                window.onclick = function (event) {
+                    if (event.target === addCardModal) {
+                        addCardModal.style.display = 'none';
+                    }
+                }
+
+                document.getElementById('cardButtonOk').addEventListener('click', function () {
+
+                    var inputCard = document.getElementById('cardInput');
+                    var inputCardValue = inputCard.value;
+
+                    if (inputCardValue !== '' && inputCardValue !== null) {
+
+                        self.addCard(new Card(inputCardValue));
+                        addCardModal.style.display = 'none';
+                        inputCard.value = '';
+
+                    } else if (inputCardValue === '') {
+                        alert('You have to write something!');
+                    }
+                });
             }
         });
     }
@@ -58,20 +95,23 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     function Card(description) {
-        
+
         var self = this;
         this.id = randomString();
         this.description = description;
-        
-        
+
         if (description !== null && description !== '') {
-            this.element = generateTemplate('card-template', {description: this.description}, 'li');
+            this.element = generateTemplate('card-template', {
+                description: this.description
+            }, 'li');
         } else if (description === null) {
             alert('No problem :)');
         } else if (description === '') {
             alert('You have to write something!');
         }
-        
+
+        this.element.querySelector('.card').style.backgroundColor = '#' + randomColorCard();
+
         this.element.querySelector('.card').addEventListener('click', function (event) {
             event.stopPropagation();
             if (event.target.classList.contains('btn-delete')) {
@@ -97,20 +137,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function initSortable(id) {
         var el = document.getElementById(id);
-        var sortable = Sortable.create(el, {group: 'kanban', sort: true});
+        var sortable = Sortable.create(el, {
+            group: 'kanban',
+            sort: true
+        });
     }
 
     document.querySelector('#board .create-column').addEventListener('click', function () {
-        
-        var name = prompt('Enter a column name');
-        
-        if (name !== '' && name !== null) {
-            var column = new Column(name);
+
+        addColumnModal.style.display = 'block';
+
+        closeColumnModal.onclick = function () {
+            addColumnModal.style.display = 'none';
+        }
+        window.onclick = function (event) {
+            if (event.target === addColumnModal) {
+                addColumnModal.style.display = 'none';
+            }
+        }
+    });
+
+    document.getElementById('columnButtonOk').addEventListener('click', function () {
+
+        var inputColumn = document.getElementById('columnInput');
+        var inputColumnValue = inputColumn.value;
+
+        if (inputColumnValue !== '' && inputColumnValue !== null) {
+
+            var column = new Column(inputColumnValue);
+
             board.addColumn(column);
-        } else if (name === '') {
+            addColumnModal.style.display = 'none';
+            inputColumn.value = '';
+
+        } else if (inputColumnValue === '') {
             alert('You have to write something!');
-        } else if (name === null) {
-            alert('No problem :)');
         }
     });
 
